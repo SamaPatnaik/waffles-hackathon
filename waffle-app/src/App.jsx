0e5c743e3cc5ccb0f3a97c5e2312050f23f8d136
Sama <samapatnaik22@gmail.com>
2024-06-22 19:02:06 -0700

    updates on the landing page, but need to add functionality (help)

--- waffle-app/src/App.jsx
+++ waffle-app/src/App.jsx
@@ -8,10 +8,11 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
-  return <div></div>;
   return (
     <div>
+      <Navbar/>
+      <LandingPage />
+      {/* <MainPage /> */}
     </div>
   );
 }
--- waffle-app/src/components/LandingPage.jsx
+++ waffle-app/src/components/LandingPage.jsx
@@ -3,8 +3,23 @@ import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Empower your health with the support you deserve</h1>
+    <div className='lpgbody'>
+      <div className='content'>
+          <h1>Empower your health with the support <span className='deserve'>you deserve.</span></h1>
+          {/* <p>It's easy! Connect with professionals in your area tailored to your preferences,
+      & make lifelong friends who understand you.</p>
+      <button>sign in</button> */}
+      </div>
+
+      <div className='para'>
+      <p>It's easy! Connect with professionals in your area tailored to your preferences,
+      & make lifelong friends who understand you.</p>
+      <button>try chatbot</button>
+      </div> 
+
+      {/* <div className='signin'>
+        <button>sign in</button>
+      </div> */}
     </div>
   );
 }
