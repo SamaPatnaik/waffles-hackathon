f95d0c838246a1a5589f4cdab437afa6908caa3a
Akshaya Varakunan <139722405+ladybugv@users.noreply.github.com>
2024-06-23 01:31:38 -0400

    small home page edits

--- waffle-app/src/components/LandingPage.jsx
+++ waffle-app/src/components/LandingPage.jsx
@@ -1,33 +1,37 @@
 import React from "react";
 import "./LandingPage.css";
+import pic from "../assets/homepage-image.png";
 
 const LandingPage = ({ callback }) => {
   return (
     <div className="lpgbody">
-      <div className="content">
-        <h1>
-          Empower your health with the support{" "}
-          <span className="deserve">you deserve.</span>
-        </h1>
-        {/* <p>It's easy! Connect with professionals in your area tailored to your preferences,
+      <div className="left-wrapper">
+        <div className="content">
+          <h1>
+            Empower your health with the support{" "}
+            <span className="deserve">you deserve.</span>
+          </h1>
+          {/* <p>It's easy! Connect with professionals in your area tailored to your preferences,
       & make lifelong friends who understand you.</p>
       <button>sign in</button> */}
+        </div>
+
+        <div className="para">
+          <p>
+            It's easy! Connect with professionals in your area tailored to your
+            preferences, & make lifelong friends who understand you.
+          </p>
+          <button
+            onClick={() => {
+              callback();
+            }}
+          >
+            try chatbot
+          </button>
+        </div>
       </div>
 
-      <div className="para">
-        <p>
-          It's easy! Connect with professionals in your area tailored to your
-          preferences, & make lifelong friends who understand you.
-        </p>
-        <button
-          onClick={() => {
-            callback();
-          }}
-        >
-          try chatbot
-        </button>
-      </div>
-
+      <img src={pic} className="home-image"></img>
       {/* <div className='signin'>
         <button>sign in</button>
       </div> */}
