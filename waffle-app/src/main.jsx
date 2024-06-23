b5fed311a00c7651462096cb851df0301932e527
Sama <samapatnaik22@gmail.com>
2024-06-22 23:17:06 -0700

    installed react-router

--- waffle-app/src/main.jsx
+++ waffle-app/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter, 
+  RouterProvider, 
+  Route, 
+} from "react-router-dom";
 import App from './App.jsx'
+import About from './pages/About.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>,
 )
+
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />
+}, 
+{
+  path: "/About",
+  element: <App />
+}
+]);
