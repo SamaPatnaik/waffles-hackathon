65550a1d663077a8b3b62d355699d35364e5a132
Sama <samapatnaik22@gmail.com>
2024-06-23 04:48:37 -0700

    about page --updates

--- waffle-app/src/pages/About.jsx
+++ waffle-app/src/pages/About.jsx
@@ -13,27 +13,29 @@ const About = () => {
         <div className="profile">
           <img src={prof1} alt="Profile 1" />
           <h1 className="prof1">discord: papichulo0244</h1>
-          <ul><li>responsible for the normalcy in naming things</li></ul>
-          <ul><li>anime fan who asks a lot of questions</li></ul>
-          <ul><li>University of British Columbia</li></ul>
+          <ul>
+            <li>responsible for the normalcy in naming things</li>
+          <li>anime fan who asks a lot of questions</li>
+          <li>University of British Columbia</li></ul>
         </div>
       </div>
       <div className="profile-wrapper">
         <div className="profile">
           <img src={prof2} alt="Profile 2" />
           <h1 className="prof2">discord: _theladybug</h1>
-          <ul><li>bro's entire personality is making bad puns (rlly smart tho)</li></ul>
-          <ul><li>lightning speed at what she does - kachow</li></ul>
-          <ul><li>University of Waterloo</li></ul>
+          <ul>
+            <li>bro's entire personality is making bad puns (rlly smart tho)</li>
+          <li>lightning speed at what she does - kachow</li>
+          <li>University of Waterloo</li></ul>
         </div>
       </div>
       <div className="profile-wrapper">
         <div className="profile">
           <img src={prof3} alt="Profile 3" />
           <h1 className="prof3">discord: .nsp.</h1>
-          <ul><li>beefy resume, devpost and github (very helpful and patient)</li></ul>
-          <ul><li>god at backend (again, rlly smart)</li></ul>
-          <ul><li>University of Maryland</li></ul>
+          <ul><li>beefy resume, devpost and github (very helpful and patient)</li>
+          <li>god at backend (again, rlly smart)</li>
+          <li>University of Maryland</li></ul>
         </div>
       </div>
     </div>
